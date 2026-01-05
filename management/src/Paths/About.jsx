63b1013eaa360f1a51886be5c2969a2224d4eb70
Raphael utenge <raphaelutenge@gmail.com>
2026-01-05 15:45:26 -0800

    Move repository to parent folder to include all project files

---
+++ management/src/Paths/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      God is faithfully
+    </div>
+  )
+}
+
+export default About
